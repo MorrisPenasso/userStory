556b77530fd09f7a09df44e30faff7083fb0cf5d
MorrisPenasso <morrispenasso@gmail.com>
2017-03-04 11:50:46 +0100

    Added login api, function that create a token when i called login api, middleware function that crypt password before that the document is saved into the database and create a function comparePassword that compare passwords for the login

--- modules/user.js
+++ modules/user.js
@@ -1,12 +1,43 @@
 var mongo = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
 
 var Schema = mongo.Schema;
 
+//create a new schema
 var userSchema = new Schema({
 
     name: String,
     username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true }
 });
+
+userSchema.pre("save", function (next) {
+
+    //refer at userSchema
+    var user = this;
+
+    //hash password of document that use this schema
+    bcrypt.hash(user.password, null, null, function (err, hashed) {
+
+        if (err) {
+            throw err;
+        } else {
+            user.password = hashed;
+
+            //next api
+            return next();
+        }
+    })
+});
+
+userSchema.methods.comparePassword = function (password) {
+
+    //refer at userSchema
+    var user = this;
+
+    //return method of bcryot library that compare two string: original password and password hashed
+    return bcrypt.compareSync(password, user.password);
+
+};
 
 module.exports = mongo.model("User", userSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,24 @@
 var User = require("../modules/user");
 var mongo = require("mongoose");
+var config = require("../config");
+
+var jsonWebToken = require("jsonWebToken");
+
+var secretKey = config.secretKey;
 
 module.exports = function (express) {
+
+    function createToken(user) {
+
+        var token = jsonWebToken.sign({
+
+            _id: user._id,
+            name: user.name,
+            username: user.username
+        }, secretKey);
+
+        return token;
+    }
 
     var api = express.Router();
 
@@ -40,5 +57,35 @@ module.exports = function (express) {
             }
         })
     });
+
+    api.post("/login", function (req, res) {
+
+        User.findOne({ username: req.body.username }).select("password").exec(function (err, usernameFinded) {
+
+            if (err) {
+                throw err;
+            }
+
+            if (!usernameFinded) {
+                res.send("This user does not exist!!");
+            } else {
+
+                var validPassword = usernameFinded.comparePassword(req.body.password);
+
+                if (!validPassword) {
+                    res.send("The password is not correct!!");
+                } else {
+
+                    var token = createToken(usernameFinded);
+                    res.send({
+                        success: true,
+                        message: "Successfully login",
+                        token: token
+                    })
+                }
+            }
+        })
+    });
+
     return api;
 }
